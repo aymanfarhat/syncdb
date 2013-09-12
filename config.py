9898de6fd1327f7d746e828d1f1e39ba238303aa
aymanfarhat <ayman.farhat1@gmail.com>
2013-09-12 22:23:46 +0300

    Config values in config file

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+dumpfile = 'dump.sql'
+
+# Remote
+server_address = ""
+server_port = 22
+server_username = ""
+server_password = ""
+
+server_db_name = ""
+server_db_username = ""
+server_db_password = ""
+
+# Local
+local_db_name = ""
+local_db_username = ""
+local_db_password = ""
 
--- syncdb.py
+++ syncdb.py
@@ -1,28 +1,12 @@
 import paramiko
 import os
-
-dumpfile = 'dump.sql'
-
-# Remote
-server_address = ""
-server_port = 22
-server_username = ""
-server_password = ""
-
-server_db_name = ""
-server_db_username = ""
-server_db_password = ""
-
-# Local
-local_db_name = ""
-local_db_username = ""
-local_db_password = ""
+import config
 
 print "Connecting to host..."
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(server_address, port=server_port, username=server_username, password=server_password)
+client.connect(config.server_address, port=config.server_port, username=config.server_username, password=config.server_password)
 
 print "Dumping database..."
 
@@ -37,14 +21,14 @@ def ssh(cmd):
 
     return(list(out))
 
-dump = ssh("mysqldump -u {0} -p{1} {2} --skip-comments".format(server_db_username, server_db_password, server_db_name))
+dump = ssh("mysqldump -u {0} -p{1} {2} --skip-comments".format(config.server_db_username, config.server_db_password, config.server_db_name))
 
 print "Saving to dump.sql..."
 
-file = open(dumpfile, 'w')
+file = open(config.dumpfile, 'w')
 file.write("".join(dump))
 file.close()
 
 print "Importing to local database..."
 
-os.system("mysql -u {0} {1} < {2}",format(local_db_username, local_db_name, dumpfile))
+os.system("mysql -u {0} {1} < {2}",format(config.local_db_username, config.local_db_name, config.dumpfile))
