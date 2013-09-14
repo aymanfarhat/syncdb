c35f854800fc06c5b53103d4eeb8971c8fedfa96
aymanfarhat <ayman.farhat1@gmail.com>
2013-09-14 22:28:24 +0300

    Use json config and allow multiple data sources

--- syncdb.py
+++ syncdb.py
@@ -1,14 +1,7 @@
+from datetime import datetime as date
 import paramiko
 import os
-import config
-
-print "Connecting to host..."
-
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(config.server_address, port=config.server_port, username=config.server_username, password=config.server_password)
-
-print "Dumping database..."
+import json
 
 def ssh(cmd):
     out = []
@@ -19,14 +12,44 @@ def ssh(cmd):
 
     return out
 
-dump = ssh("mysqldump -u {0} --password='{1}' {2} --skip-comments".format(config.server_db_username, config.server_db_password, config.server_db_name))
+# load the config
+config_json = open('config.json')
+config_list = json.load(config_json)
+config_json.close()
 
-print "Saving to {0}".format(dumpfile)
+for config in config_list:
+    print "Connecting to host..."
 
-file = open(config.dumpfile, 'w')
-file.write("".join(dump))
-file.close()
+    remote_address = config["remote"]["address"] 
+    remote_port = config["remote"]["port"]
+    remote_user = config["remote"]["username"]
+    remote_pass = config["remote"]["password"]
 
-print "Importing to local database..."
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(remote_address, port=remote_port, username=remote_user, password=remote_pass)
 
-os.system("mysql -u {0} {1} < {2}".format(config.local_db_username, config.local_db_name, config.dumpfile))
+    print "Dumping database..."
+
+    remote_dbname = config["remote"]["db_name"]
+    remote_db_username = config["remote"]["db_username"]
+    remote_db_pass = config["remote"]["db_password"]
+
+    dump = ssh("mysqldump -u {0} --password='{1}' {2} --skip-comments".format(remote_db_username, remote_db_pass, remote_dbname))
+
+    today = date.today().strftime("%y-%m-%d")
+    dumpfile = "{0}_{1}.sql".format(remote_dbname, today)
+
+    print "Saving to {0}".format(dumpfile)
+
+    file = open(dumpfile, 'w')
+    file.write("".join(dump))
+    file.close()
+
+    print "Importing to local database..."
+
+    local_dbname = config["local"]["db_name"]
+    local_db_username = config["local"]["db_username"]
+    local_db_pass = config["local"]["db_password"]
+
+    os.system("mysql -u {0} {1} < {2}".format(local_db_username, local_dbname, dumpfile))
